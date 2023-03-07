82f35cf9c072ea3ccef3f563b79c5003a2d7413f
PatchesTheWright <jonathan.valbe.jb@gmail.com>
2023-03-07 02:04:55 -0300

    feat: Adicionado variaveis globais no SCSS e 1 imagem de fundo

--- src/components/calculator/index.tsx
+++ src/components/calculator/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Buttons } from '../buttons';
 import { Header } from '../header';
+import "./style.css"
 
 export interface IState {
     calculator: {
