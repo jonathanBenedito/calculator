73483edf911c335e7a7776901c207c8b524c59a4
PatchesTheWright <jonathan.valbe.jb@gmail.com>
2023-03-09 01:57:51 -0300

    feat: Implemented test units

--- src/components/buttons/index.tsx
+++ src/components/buttons/index.tsx
@@ -1,6 +1,7 @@
 import { IState as Props } from "../calculator";
 import { evaluate } from 'mathjs';
 import './style.css'
+import { useEffect, useState } from "react";
 
 interface IButtonProps {
     calculator: Props["calculator"],
@@ -9,9 +10,22 @@ interface IButtonProps {
 
 export const Buttons: React.FC<IButtonProps> = ({ calculator, setCalculator }) => {
 
-    const handleNumClick = (num: number | string) => {
-        setCalculator({ ...calculator, input: calculator.input + num })
-    }
+    const handleNumClick = (num: number | string): void => {
+        const input = calculator.input;
+        const result = calculator.result;
+
+        if (result !== 0) {
+            setCalculator({
+                ...calculator,
+                input: result.toString() + num,
+                result: 0
+            });
+            return 
+        } else if (num.toString().match(/[+\-*/]/) && input === "") {
+            return
+        }
+        setCalculator({ ...calculator, input: input + num });
+    };
 
     const handleClearClick = (): void => {
         setCalculator({ result: 0, input: "" })
@@ -21,10 +35,27 @@ export const Buttons: React.FC<IButtonProps> = ({ calculator, setCalculator }) =
         const res = evaluate(calculator.input);
 
         setCalculator({
-            input: "",
+            input: '',
             result: res
         })
     }
+
+    const inputStartsWithOperator = (input: string) => {
+        const firstChar = input.charAt(0)
+        return firstChar === '+' || firstChar === '-' || firstChar === '*' || firstChar === '/';
+    }
+
+    const inputEndsWithOperator = (input: string) => {
+        const lastChar = input.charAt(input.length - 1)
+        return lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/';
+    }
+
+    const [equalIsDisabled, setEqualIsDisabled] = useState<boolean>(false)
+
+    useEffect(() => {
+        const input = calculator.input
+        setEqualIsDisabled(inputStartsWithOperator(input) || inputEndsWithOperator(input))
+    }, [calculator.input])
 
     return (
         <section className="buttons">
@@ -47,8 +78,13 @@ export const Buttons: React.FC<IButtonProps> = ({ calculator, setCalculator }) =
 
             <button onClick={() => handleNumClick(0)}>0</button>
             <button onClick={() => handleNumClick(".")}>.</button>
-            <button onClick={() => handleEqualClick()}>=</button>
-            <button className="blue" onClick={() => handleNumClick("+")}>+</button>
+            <button onClick={() => handleEqualClick()} disabled={equalIsDisabled}>=</button>
+            <button
+                className="blue"
+                onClick={() => handleNumClick("+")}
+            >
+                +
+            </button>
         </section>
     )
 }
---
+++ src/components/calculator/tests/calculator.test.tsx
@@ -0,0 +1,80 @@
+import { render, screen, fireEvent } from "@testing-library/react"
+import { Buttons } from "../../buttons";
+
+describe('Buttons component', () => {
+    const setCalculator = jest.fn();
+    const calculator = { result: 0, input: '2+2' }
+    const setup = () => render(<Buttons {...{ calculator, setCalculator }} />)
+
+    test('Should render all the buttons correctly', () => {
+        setup();
+
+        expect(screen.getByRole('button', { name: 'C' })).toBeInTheDocument();
+
+        expect(screen.getByRole('button', { name: '7' })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: '8' })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: '9' })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: '/'})).toBeInTheDocument();
+
+        expect(screen.getByRole('button', { name: '4' })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: '5' })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: '6' })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: 'x' })).toBeInTheDocument();
+
+        expect(screen.getByRole('button', { name: '1' })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: '2' })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: '3' })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: '-' })).toBeInTheDocument();
+
+        expect(screen.getByRole('button', { name: '0' })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: '.' })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: '=' })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: '+' })).toBeInTheDocument();
+    })
+
+    test('Should display the correct input when a number button is clicked', () => {
+        setup();
+
+        const button_1 = screen.getByRole('button', { name: '1' });
+        fireEvent.click(button_1);
+
+        expect(setCalculator).toHaveBeenCalledWith({ result: 0, input: '2+21' });
+    })
+
+    test('Should not register the input when a operator button is clicked with empty input', () => {
+        render(<Buttons calculator={{ result: 0, input: '' }} {...{ setCalculator }} />);
+
+        const button_plus = screen.getByRole('button', { name: '+' });
+        fireEvent.click(button_plus);
+
+        expect(setCalculator).not.toHaveBeenCalled();
+    })
+
+    test('When the clear button is clicked, the input field of the calculator should be cleared', () => {
+        setup();
+
+        const clearButton = screen.getByRole('button', { name: 'C' });
+        fireEvent.click(clearButton);
+
+        expect(setCalculator).toHaveBeenCalledWith({ result: 0, input: '' });
+    })
+
+    test('Should calculate the expression correctly', () => {
+        setup();
+        
+        const equalButton = screen.getByRole('button', { name: '=' });
+        fireEvent.click(equalButton);
+
+        expect(setCalculator).toHaveBeenCalledWith({result: 4, input: ''})
+    })
+
+    test('Should not calculate if the input ends with any operator', () => {
+        render(<Buttons calculator={{ result: 0, input: '2+' }} {...{ setCalculator }} />);
+
+        const equalButton = screen.getByRole('button', { name: '=' });
+        fireEvent.click(equalButton);
+
+        expect(setCalculator).not.toHaveBeenCalled();
+    })
+})
+
